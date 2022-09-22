709accc9b30440bf6bc452dd8273020b1c37e50d
PGA <paulgadam@gmail.com>
2022-09-22 01:02:28 -0600

    updated model and CRUD with README

--- Game/game_pkg/c_r_u_d.py
+++ Game/game_pkg/c_r_u_d.py
@@ -4,108 +4,128 @@
 
 from game_pkg import exceptions as mvc_exc
 
+import sqlite3
+
+DB_name = 'myDB'
+
+
+def connect_to_db(db=None):
+    """Connect to a sqlite DB. Create the database if there isn't one yet.
+
+    Open a connection to a SQLite DB (either a DB file or an in-memory DB).
+    When a database is accessed by multiple connections, and one of the
+    processes modifies the database, the SQLite database is locked until that
+    transaction is committed.
+
+    Parameters
+    ----------
+    db : str
+        database name (without .db extension). If None, create an In-Memory DB.
+
+    Returns
+    -------
+    connection : sqlite3.Connection
+        connection object
+    """
+    if db is None:
+        mydb = ':memory:'
+        print('New connection to in-memory SQLite DB...')
+    else:
+        mydb = '{}.db'.format(db)
+        print('New connection to SQLite DB...')
+    connection = sqlite3.connect(mydb)
+    return connection
+
+
+
+
+
 '''backend list'''
 
 # global Martial Arts opponents list, for creating, updating, etc...
-opponent_list = list()
+# opponent_list = list()
 
 
 
 
-# create char list
+# # create char list
 
 
-def create_chars(game_chars):
-    global opponent_list
-    opponent_list = game_chars
+# def create_chars(game_chars):
+#     global opponent_list
+#     opponent_list = game_chars
 
-# create individual char
-
-
-def create_char(name, ac, damage, hp, to_hit):
-    global opponent_list
-    results = list(filter(lambda x: x["name"] == name, opponent_list))
-    if results:
-        raise mvc_exc.CharAlreadyStored(f"{name} already stored!")
-    else:
-        opponent_list.append({"name": name,
-                                "ac": ac,
-                                "damage": damage, 
-                                "hp": hp, 
-                                "to_hit": to_hit})
-
-# retrive and get char
+# # create individual char
 
 
-def read_char(name):
-    global opponent_list
-    opponents = list(filter(lambda x: x['name'] == name, opponent_list))
-    if opponents:
-        return opponents[0]
-    else:
-        raise mvc_exc.CharNotStored(
-            f"Can't read {name} because it's not stored"
-        )
+# def create_char(name, ac, damage, hp, to_hit):
+#     global opponent_list
+#     results = list(filter(lambda x: x["name"] == name, opponent_list))
+#     if results:
+#         raise mvc_exc.CharAlreadyStored(f"{name} already stored!")
+#     else:
+#         opponent_list.append({"name": name,
+#                                 "ac": ac,
+#                                 "damage": damage, 
+#                                 "hp": hp, 
+#                                 "to_hit": to_hit})
 
-# read char list
-
-
-def read_chars():
-    global opponent_list
-    return [list_item for list_item in opponent_list]
-
-# update individual char
+# # retrive and get char
 
 
-def update_char(name, ac, damage, hp, to_hit):
-    global opponent_list
-    # Python 3.x removed tuple parameters unpacking (PEP 3113), so we have to do it manually (index_x is a tuple, index_oppponent is a list of tuples)
-    index_oppponent = list(
-        filter(lambda index_x: index_x[1]["name"]
-               == name, enumerate(opponent_list))
-    )
-    if index_oppponent:
-        i, item_to_update = index_oppponent[0][0], index_oppponent[0][1]
-        opponent_list[i] = {"name": name,
-                            "ac": ac,
-                            "damage": damage, 
-                            "hp": hp, 
-                            "to_hit": to_hit}
-    else:
-        raise mvc_exc.CharNotStored(
-            f"Can't update {name} because it's not stored"
-        )
+# def read_char(name):
+#     global opponent_list
+#     opponents = list(filter(lambda x: x['name'] == name, opponent_list))
+#     if opponents:
+#         return opponents[0]
+#     else:
+#         raise mvc_exc.CharNotStored(
+#             f"Can't read {name} because it's not stored"
+#         )
 
-# delete char
+# # read char list
 
 
-def delete_char(name):
-    global opponent_list
-    # From architect: Python 3.x removed tuple parameters unpacking (PEP 3113), so we have to do it manually (i_x is a tuple, idxs_items is a list of tuples)
-    index_oppponent = list(
-        filter(lambda index_x: index_x[1]["name"] == name, enumerate(opponent_list)))
-    if index_oppponent:
-        i, item_to_delete = index_oppponent[0][0], index_oppponent[0][1]
-        del opponent_list[i]
-    else:
-        raise mvc_exc.CharNotStored(
-            f"Can't delete {name} because it's not stored."
-        )
+# def read_chars():
+#     global opponent_list
+#     return [list_item for list_item in opponent_list]
+
+# # update individual char
 
 
-# opponents: dict = [{"name": "player1", "arena_level": 1, "damage": 7, "hp": 25, "to_hit": 35,
-#                     "strength": 15, "dexterity": 15, "constitution": 15},
-#                    {"name": "master", "arena_level": 4, "damage": 50, "hp": 1000, "to_hit": 75,
-#                     "strength": 15, "dexterity": 15, "constitution": 15},
-#                    {"name": "warrior", "arena_level": 1, "damage": 5, "hp": 15, "to_hit": 25,
-#                     "strength": 15, "dexterity": 15, "constitution": 15},
-#                    {"name": "veteran", "arena_level": 2, "damage": 8,
-#                     "hp": 1000, "to_hit": 30,
-#                     "strength": 15, "dexterity": 15, "constitution": 15},
-#                    {"name": "grandmaster", "arena_level": 5,
-#                     "damage": 60, "arena_level": 1250, "to_hit": 80,
-#                     "strength": 15, "dexterity": 15, "constitution": 15},
-#                    {"name": "elite", "arena_level": 3, "damage": 15, "hp": 100, "to_hit": 35,
-#                     "strength": 15, "dexterity": 15, "constitution": 15}]
+# def update_char(name, ac, damage, hp, to_hit):
+#     global opponent_list
+#     # Python 3.x removed tuple parameters unpacking (PEP 3113), so we have to do it manually (index_x is a tuple, index_oppponent is a list of tuples)
+#     index_oppponent = list(
+#         filter(lambda index_x: index_x[1]["name"]
+#                == name, enumerate(opponent_list))
+#     )
+#     if index_oppponent:
+#         i, item_to_update = index_oppponent[0][0], index_oppponent[0][1]
+#         opponent_list[i] = {"name": name,
+#                             "ac": ac,
+#                             "damage": damage, 
+#                             "hp": hp, 
+#                             "to_hit": to_hit}
+#     else:
+#         raise mvc_exc.CharNotStored(
+#             f"Can't update {name} because it's not stored"
+#         )
 
-#player_items: dict = [ {"name": "player1", "hd": 1, "damage": 7, "hp": 25, "to_hit": 35}]
+# # delete char
+
+
+# def delete_char(name):
+#     global opponent_list
+#     # From architect: Python 3.x removed tuple parameters unpacking (PEP 3113), so we have to do it manually (i_x is a tuple, idxs_items is a list of tuples)
+#     index_oppponent = list(
+#         filter(lambda index_x: index_x[1]["name"] == name, enumerate(opponent_list)))
+#     if index_oppponent:
+#         i, item_to_delete = index_oppponent[0][0], index_oppponent[0][1]
+#         del opponent_list[i]
+#     else:
+#         raise mvc_exc.CharNotStored(
+#             f"Can't delete {name} because it's not stored."
+#         )
+
+
--- Game/game_pkg/game_model.py
+++ Game/game_pkg/game_model.py
@@ -1,38 +1,84 @@
 from game_pkg import c_r_u_d
-
+from game_pkg import exceptions as mvc_exc
 '''model'''
-class Character_Model(object):
+
+
+class ModelSQLite(object):
 
     def __init__(self, character_list_elements):
-        self._char_type = "warrior"
+        self._char_type = 'Warrior'
+        self._connection = c_r_u_d.connect_to_db(c_r_u_d.DB_name)
+        c_r_u_d.create_table(self.connection, self._char_type)
         self.create_chars(character_list_elements)
+
+    @property
+    def connection(self):
+        return self._connection
 
     @property
     def char_type(self):
         return self._char_type
 
     @char_type.setter
-    def char_type(self, new_char_type):
+    def item_type(self, new_char_type):
         self._char_type = new_char_type
 
     def create_char(self, name, ac, damage, hp, to_hit):
-        c_r_u_d.create_char(name, ac, damage, hp, to_hit)
+        c_r_u_d.insert_char(
+            self.connection, name, ac, damage, hp, to_hit, table_name=self.char_type)
 
-
-    def create_chars(self, opponent_list):
-        c_r_u_d.create_chars(opponent_list)
+    def create_chars(self, chars):
+        c_r_u_d.insert_chars(
+            self.connection, chars, table_name=self.char_type)
 
     def read_char(self, name):
-        return c_r_u_d.read_char(name)
+        return c_r_u_d.select_char(
+            self.connection, name, table_name=self.char_type)
 
     def read_chars(self):
-        return c_r_u_d.read_chars()
+        return c_r_u_d.select_all(
+            self.connection, table_name=self.char_type)
 
     def update_char(self, name, ac, damage, hp, to_hit):
-        c_r_u_d.update_char(name, ac, damage, hp, to_hit)
-
-    def return_char_item(self, name, ac, damage, hp, to_hit):
-        c_r_u_d.read_char(name)
+        c_r_u_d.update_char(
+            self.connection, name, ac, damage, hp, to_hit, table_name=self.char_type)
 
     def delete_char(self, name):
-        c_r_u_d.delete_char(name)
+        c_r_u_d.delete_char(
+            self.connection, name, table_name=self.char_type)
+
+# class Character_Model(object):
+
+#     def __init__(self, character_list_elements):
+#         self._char_type = "warrior"
+#         self.create_chars(character_list_elements)
+
+#     @property
+#     def char_type(self):
+#         return self._char_type
+
+#     @char_type.setter
+#     def char_type(self, new_char_type):
+#         self._char_type = new_char_type
+
+#     def create_char(self, name, ac, damage, hp, to_hit):
+#         c_r_u_d.create_char(name, ac, damage, hp, to_hit)
+
+
+#     def create_chars(self, opponent_list):
+#         c_r_u_d.create_chars(opponent_list)
+
+#     def read_char(self, name):
+#         return c_r_u_d.read_char(name)
+
+#     def read_chars(self):
+#         return c_r_u_d.read_chars()
+
+#     def update_char(self, name, ac, damage, hp, to_hit):
+#         c_r_u_d.update_char(name, ac, damage, hp, to_hit)
+
+#     def return_char_item(self, name, ac, damage, hp, to_hit):
+#         c_r_u_d.read_char(name)
+
+#     def delete_char(self, name):
+#         c_r_u_d.delete_char(name)
